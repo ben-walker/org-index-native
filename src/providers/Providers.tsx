2e041ac26e789a76db55a4b4fdd71708b7f0c1a4
Ben Walker <b6nw4r@gmail.com>
2022-02-18 13:05:53 -0500

    Setup react navigation provider

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,8 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
+import { NavigationContainer } from "@react-navigation/native";
 import { FC } from "react";
 
 import { client } from "./apollo";
 
 export const Providers: FC = ({ children }) => (
-  <ApolloProvider client={client}>{children}</ApolloProvider>
+  <ApolloProvider client={client}>
+    <NavigationContainer>{children}</NavigationContainer>
+  </ApolloProvider>
 );
