5b4679bd6e1d747a76e2862ad00729f84c3f99e7
Ben Walker <b6nw4r@gmail.com>
2022-02-26 18:03:27 -0500

    Fix default graph url

--- src/env/env.ts
+++ src/env/env.ts
@@ -2,6 +2,6 @@ import Constants from "expo-constants";
 
 import { ManifestExtra } from "./types";
 
-export const { GRAPH_URI = "http://localhost:4000" } = <ManifestExtra>(
+export const { GRAPH_URI = "http://localhost:4000/graphql" } = <ManifestExtra>(
   Constants.manifest?.extra
 );
