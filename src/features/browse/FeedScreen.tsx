86076bcbb99dd95918faa725ab6ed7ee5baa8252
Ben Walker <b6nw4r@gmail.com>
2022-03-05 11:04:16 -0500

    Fix FlatList

--- src/features/browse/FeedScreen.tsx
+++ src/features/browse/FeedScreen.tsx
@@ -2,9 +2,24 @@ import { useQuery } from "@apollo/client";
 import { FlatList } from "react-native";
 
 import { FeedDocument } from "../../generated/graphql";
+import { LIST_END_REACHED_THRESHOLD, Text } from "../common";
 
 export const FeedScreen = () => {
-  const { loading } = useQuery(FeedDocument);
+  const { data, loading, refetch } = useQuery(FeedDocument);
 
-  return <FlatList refreshing={loading} />;
+  // TODO: use callbacks for FlatList props, add pagination
+  return (
+    <FlatList
+      data={data?.organizations}
+      keyExtractor={({ id }) => id}
+      onEndReachedThreshold={LIST_END_REACHED_THRESHOLD}
+      onRefresh={() =>
+        void (async () => {
+          await refetch();
+        })
+      }
+      refreshing={loading}
+      renderItem={({ item: { name } }) => <Text variant="body">{name}</Text>}
+    />
+  );
 };
