6309dc91e272599a2effa64ef7b866b8372be25f
Ben Walker <b6nw4r@gmail.com>
2022-03-15 21:07:05 -0400

    Add component for organizations in the feed

---
+++ src/features/browse/FeedItemOrganization.tsx
@@ -0,0 +1,28 @@
+import { useNavigation } from "@react-navigation/native";
+import { useCallback } from "react";
+import { TouchableOpacity } from "react-native";
+
+import { Text } from "../common";
+import { BrowseStackNavigation } from "./navigation";
+import { FeedOrganization } from "./types";
+
+type Props = {
+  organization: FeedOrganization;
+};
+
+export const FeedItemOrganization = ({ organization: { id, name } }: Props) => {
+  const navigation = useNavigation<BrowseStackNavigation<"Feed">>();
+
+  const pressHandler = useCallback(
+    () => navigation.navigate("Organization", { name }),
+    [navigation, name]
+  );
+
+  return (
+    <TouchableOpacity onPress={pressHandler}>
+      <Text variant="body">
+        {id} {name}
+      </Text>
+    </TouchableOpacity>
+  );
+};
--- src/features/browse/FeedScreen.tsx
+++ src/features/browse/FeedScreen.tsx
@@ -1,11 +1,10 @@
 import { useQuery } from "@apollo/client";
-import { useNavigation } from "@react-navigation/native";
 import { useCallback } from "react";
-import { FlatList, ListRenderItem, TouchableHighlight } from "react-native";
+import { FlatList, ListRenderItem } from "react-native";
 
 import { FeedDocument } from "../../generated/graphql";
-import { LIST_END_REACHED_THRESHOLD, LIST_PAGE_SIZE, Text } from "../common";
-import { BrowseStackNavigation } from "./navigation";
+import { LIST_END_REACHED_THRESHOLD, LIST_PAGE_SIZE } from "../common";
+import { FeedItemOrganization } from "./FeedItemOrganization";
 import { FeedOrganization } from "./types";
 
 export const FeedScreen = () => {
@@ -13,21 +12,11 @@ export const FeedScreen = () => {
     variables: { take: LIST_PAGE_SIZE },
   });
 
-  const navigation = useNavigation<BrowseStackNavigation<"Feed">>();
-
   const keyExtractor = useCallback(({ id }: FeedOrganization) => id, []);
 
   const renderItem: ListRenderItem<FeedOrganization> = useCallback(
-    ({ item: { id, name } }) => (
-      <TouchableHighlight
-        onPress={() => navigation.navigate("Organization", { name })}
-      >
-        <Text variant="body">
-          {id} {name}
-        </Text>
-      </TouchableHighlight>
-    ),
-    [navigation]
+    ({ item }) => <FeedItemOrganization organization={item} />,
+    []
   );
 
   const handleRefresh = useCallback(() => void refetch(), [refetch]);
