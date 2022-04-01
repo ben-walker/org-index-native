0d8b569ccd53c7d015bacddb970029bbd6c85d1e
Ben Walker <b6nw4r@gmail.com>
2022-04-01 18:29:20 -0400

    Use apollo links in graphql client

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,9 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import { GRAPH_URI } from "../env";
 import { typePolicies } from "./type-policies";
 
+const authLink = setContext(
+  (_, { headers }: { headers: Record<string, unknown> }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: "",
+      },
+    };
+  }
+);
+
+const httpLink = createHttpLink({
+  uri: GRAPH_URI,
+});
+
 export const client = new ApolloClient({
   cache: new InMemoryCache({ typePolicies }),
-  uri: GRAPH_URI,
+  link: authLink.concat(httpLink),
 });
