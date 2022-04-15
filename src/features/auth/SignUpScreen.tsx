75bb09bccd62893d6ede50647f6dd3282075cfc3
Ben Walker <b6nw4r@gmail.com>
2022-04-15 17:22:54 -0400

    Implement sign up screen

--- src/features/auth/SignUpScreen.tsx
+++ src/features/auth/SignUpScreen.tsx
@@ -1,1 +1,62 @@
-export const SignUpScreen = () => null;
+import { useMutation } from "@apollo/client";
+import { setItemAsync } from "expo-secure-store";
+import { Controller, useForm } from "react-hook-form";
+import { Button, ScrollView, TextInput } from "react-native";
+
+import { SignUpDocument, SignUpInput } from "../../generated/graphql";
+import { Text } from "../common";
+import { STORE_KEY_ACCESS_TOKEN } from "./constants";
+
+export const SignUpScreen = () => {
+  const [signUp] = useMutation(SignUpDocument);
+
+  const { control, handleSubmit } = useForm<SignUpInput>();
+
+  const handleSignUp = handleSubmit(async (userData) => {
+    try {
+      const { data } = await signUp({ variables: { userData } });
+      const { accessToken } = data?.signUp || {};
+
+      if (!accessToken) {
+        return;
+      }
+
+      await setItemAsync(STORE_KEY_ACCESS_TOKEN, accessToken);
+    } catch {
+      // Ignore; error returned from useMutation
+    }
+  });
+
+  return (
+    <ScrollView>
+      <Text variant="body">Email</Text>
+      <Controller
+        control={control}
+        name="email"
+        render={({ field: { onBlur, onChange, value } }) => (
+          <TextInput onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+        rules={{ required: true }}
+      />
+      <Text variant="body">Name</Text>
+      <Controller
+        control={control}
+        name="name"
+        render={({ field: { onBlur, onChange, value } }) => (
+          <TextInput onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+        rules={{ required: true }}
+      />
+      <Text variant="body">Password</Text>
+      <Controller
+        control={control}
+        name="password"
+        render={({ field: { onBlur, onChange, value } }) => (
+          <TextInput onBlur={onBlur} onChangeText={onChange} value={value} />
+        )}
+        rules={{ required: true }}
+      />
+      <Button title="Sign Up" onPress={handleSignUp} />
+    </ScrollView>
+  );
+};
